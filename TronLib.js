50b90b0edf219b7811e94bdd4b821cd4d3ccca38
AndrewVV <vyakhirevandrew@gmail.com>
2020-04-08 19:43:21 +0300

    made updates in methods

--- TronLib.js
+++ TronLib.js
@@ -1,16 +1,10 @@
+require('dotenv').config();
 const TronWeb = require('tronweb')
 const fetch = require('node-fetch'); 
-const DecimalConverter = require('./DecimalConverter');
-const HttpProvider = TronWeb.providers.HttpProvider;
-const CryptoUtils = require("@tronscan/client/src/utils/crypto");
 
-const fullNode = new HttpProvider('https://api.shasta.trongrid.io');
-const solidityNode = new HttpProvider('https://api.shasta.trongrid.io');
-const eventServer = 'https://api.shasta.trongrid.io';
-const fullMainNode = 'https://api.trongrid.io';
-
-// testnet privkey
-const privateKey = '0560d5eed51ff19c1ae98da2a5be0ca0b3a9d71ebad10c407db0f8cb093d47b0';
+const fullNode = new TronWeb.providers.HttpProvider(process.env.TRON_TESTNET_NODE);
+const solidityNode = new TronWeb.providers.HttpProvider(process.env.TRON_TESTNET_NODE);
+const eventServer = process.env.TRON_TESTNET_NODE;
 
 class TronLib {
 	constructor(){	
@@ -18,134 +12,142 @@ class TronLib {
     		fullNode,
     		solidityNode,
     		eventServer,
-    		privateKey, 
 		);
-
-		// test
-		//this.getTxInfo('TE771U5EEQ9PAVQZsv95DqVGwjEFHaaP9y', '1562770548000')
+		// for test
+		// this.getBalance(false, process.env.TEST_ADDRESS)
+		// this.sendTransaction("THouzVw4rA4i6Y191nVmYQtXXtePMU7s2J", 1.2345)
+		// this.generateAccount()
+		// this.getTxInfo(process.env.TEST_ADDRESS, '1562770548000')
 	}
 
-	generateAccount(){
-		return new Promise(async(resolve,reject)=>{
-    	    try{
-				let url = fullMainNode+'/wallet/generateaddress'
-				let pair = await this.postMethod(url)
-				let data = {
-					address: pair.address,
-					privateKey: pair.privateKey
-				}
-				return resolve(data)
-			}catch(e){
-    	        return reject(e);
+	async generateAccount(){
+    	try{
+			const url = `${process.env.TRON_TESTNET_NODE}/wallet/generateaddress`
+			let pair = await this.postMethod(url)
+			let data = {
+				address: pair.address,
+				privateKey: pair.privateKey
 			}
-		})
+			console.log(data)
+			return data;
+		}catch(e){
+    	    return e;
+		}
 	}
 
-	getBalance(address){
-    	return new Promise(async(resolve,reject)=>{
-    	    try{
-				let balance = await this.tronWeb.trx.getBalance(address)
-				balance = this.toDecimals(balance)
-				return resolve(balance)
-    	    }catch(e){
-    	        return reject(e);
-    	    }
-		})
+	async getBalance(integer=true, address){
+    	try {
+            let balance = await this.tronWeb.trx.getBalance(address);
+            if(!integer){
+                balance = this.tronWeb.fromSun(balance);
+			}
+			console.log(balance)
+			return balance
+    	} catch (error) {
+			console.error("Error", error)
+	    }
+	}
+	
+	async sendTransaction(to, amount){
+    	try {
+            amount = this.tronWeb.toSun(amount);
+            const from = process.env.TEST_ADDRESS;
+            const privateKey = process.env.TEST_PRIVATEKEY;
+			const transaction = await this.tronWeb.transactionBuilder.sendTrx(to, amount, from);
+			const signedTransaction = await this.tronWeb.trx.sign(transaction, privateKey);
+			const result = await this.tronWeb.trx.sendRawTransaction(signedTransaction);
+            const txHash = result.transaction.txID
+            console.log('txHash', txHash)
+			return txHash;
+    	} catch (error) {
+            console.error(error);
+    	}
 	}
 
-	sendTx(to, amount){
-    	return new Promise(async(resolve,reject)=>{
-    	    try{
-				let from = CryptoUtils.pkToAddress(privateKey);
-				let transaction = await this.tronWeb.transactionBuilder.sendTrx(to, amount, from, 1);
-				let signedTransaction = await this.tronWeb.trx.sign(transaction, privateKey)
-				let result = await this.tronWeb.trx.sendRawTransaction(signedTransaction)
-				result = result.transaction.txID
-				return resolve(result)
-    	    }catch(e){
-    	        return reject(e);
-    	    }
-		})
-	}
-
-    getTxInfo(address, minTimestamp){
-    	return new Promise(async(resolve,reject)=>{
-    	    try{
-				let result = [];
-				let url = fullMainNode+'/v1/accounts/'+address+'/transactions?only_to=true&min_timestamp='+minTimestamp
-				let allTx = await this.getMethod(url)
-				allTx = allTx.data;
-				for(let txKey in allTx){
-					let tx = allTx[txKey];
-					if(tx.raw_data != undefined){
-						if(tx.raw_data.contract[0].type === "TransferContract"){
-							let hash = tx.txID;
-							let amount = tx.raw_data.contract[0].parameter.value.amount;
-							amount = this.toDecimals(amount)
-							let timestamp = tx.raw_data.timestamp;
-							let txData = await this.formatTxData(hash, amount, timestamp);
-							result.push(txData)
-						}
-					}
+    async getTxInfo(address, minTimestamp){
+    	try{
+			let result = [];
+			const url = `${process.env.TRON_TESTNET_NODE}/v1/accounts/${address}/transactions?only_to=true&min_timestamp=${minTimestamp}`;
+			let allTx = await this.getMethod(url)
+			allTx = allTx.data;
+			for(let txKey in allTx){
+				let tx = allTx[txKey];
+				if(tx.raw_data != undefined){
+					if(tx.raw_data.contract[0].type === "TransferContract"){
+						let hash = tx.txID;
+						const txFee = this.tronWeb.fromSun(tx.ret[0].fee);
+						let amount = tx.raw_data.contract[0].parameter.value.amount;
+						amount = this.tronWeb.fromSun(amount)
+						let timeStamp = tx.raw_data.timestamp;
+						timeStamp = parseInt(timeStamp/1000);
+                        const from = TronWeb.address.fromHex(tx.raw_data.contract[0].parameter.value.owner_address);
+                        const to = TronWeb.address.fromHex(tx.raw_data.contract[0].parameter.value.to_address);
+						let status = tx.ret[0].contractRet;
+						let action;
+                        if(address == to){
+                            action = "DEPOSIT";
+                        }else if(address == from){
+                            action = "SEND";
+                        }
+						const id = result.length+1;
+						let txData = this.formatTxData(timeStamp, id, action, status, amount, hash, from, to, txFee);
+                        result.push(txData);
+					} continue;
 				}
-				console.log(result)
-				return resolve(result)
-    	    }catch(e){
-    	        return reject(e);
-    	    }
-		})
+			}
+			console.log(result)
+			return result
+    	}catch(e){
+    	    console.error(e);
+    	}
 	}
 
-	formatTxData(hash, amount, timestamp){
+    formatTxData(timeStamp, id, action, status, amount, hash, from, to, txFee){
 		let txData = {
-			txHash: hash,
-			amount: amount, 
-			timestamp: timestamp
+            timeStamp,
+            id,
+            action,
+            status,
+            cryptoAmount: amount,
+            hash,
+            fromAddress: from,
+            toAddress: to,
+            txFee, 
 		};
 		return txData;
-	}
-
-	getMethod(url){
-		return new Promise(async(resolve,reject)=>{
-			try{
-				let result = await fetch(url)
-				.then(function(responce) {
-					return responce.json()
-				})
-				return resolve(result);
-			}catch(e){
-    	    	return reject(e);
-			}
-		})
-	}
-
-	postMethod(url, body={}){
-		return new Promise(async(resolve,reject)=>{
-			try{
-				let options= {
-					method: 'POST',
-                	body: JSON.stringify(body),
-                	headers: {
-						"Content-Type": "application/json"
-					}
-            	};
-				let result = await fetch(url, options)
-				.then(function(responce) {
-					return responce.json()
-				})
-				return resolve(result);
-			}catch(e){
-    	    	return reject(e);
-			}
-		})
-	}
-
-	toDecimals(amount, decimals=6){
-        return DecimalConverter.formatToDecimals(amount, decimals);
     }
-    fromDecimals(amount, decimals=6){
-        return DecimalConverter.formatFromDecimals(amount, decimals);
-    }
+
+	async getMethod(url){
+		try{
+			const result = await fetch(url)
+			.then(res => {
+				return res.json()
+			})
+			return result;
+		}catch(e){
+    		return e;
+		}
+	}
+
+	async postMethod(url, body={}){
+		try{
+			let options= {
+				method: 'POST',
+            	body: JSON.stringify(body),
+            	headers: {
+					"Content-Type": "application/json"
+				}
+        	};
+			const result = await fetch(url, options)
+			.then(res => {
+				return res.json()
+			})
+			return result;
+		}catch(e){
+    		return e;
+		}
+	}
+
 }
 
 module.exports = TronLib;
